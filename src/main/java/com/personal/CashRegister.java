4cde1ac58f3389829ac51aecef5d0918342ca904
Franco Nazareno <franco.nazareno@gmail.com>
2018-07-29 09:54:06 +0900

    Added sources

---
+++ src/main/java/com/personal/CashRegister.java
@@ -0,0 +1,11 @@
+package com.personal;
+
+public class CashRegister {
+
+    /*
+     * Main entry
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
